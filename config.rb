53cde32d7a9f0f0b5f7a249915da36121fd7bd09
Aaron Cruz <aaron@aaroncruz.com>
2014-11-17 15:51:31 +0100

    Added git deploy and some plugins for compilation

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
+activate :deploy do |deploy|
+  deploy.method = :git
+end
+
 ###
 # Compass
 ###
@@ -36,9 +40,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -56,13 +60,13 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
